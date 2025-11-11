e1157b8406747703da44e5a760f9293d043e60e3
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-11-11 12:17:04 +0600

    !

--- 00 - Intro/PH/dist/index.js
+++ 00 - Intro/PH/dist/index.js
@@ -1,8 +1,42 @@
-'use strict';
+"use strict";
 let age = 30;
 let largeNumber = 10000000;
 let fname = 'Shakib';
 let isOk = true;
 let empty;
 console.log(empty);
+let a = 4;
+let shoppingList = ['egg', 'milk', 'tea'];
+let mixedArr = [
+    'egg',
+    12,
+    'milk',
+    1,
+    'suger',
+    5,
+];
+mixedArr.push(true);
+let coord = [20, 30];
+let couple = ['husbend', 'wife'];
+let student = ['Sakib', 17];
+console.log(student[0]);
+student[0] = 'O_o';
+console.log(student[0]);
+let emptyArray = [];
+let obj = {
+    id: 1,
+    name: '',
+    retireDate: (date) => {
+        console.log(date);
+    },
+};
+console.log(obj);
+const person = {
+    id: 15,
+    name: 'Sakib',
+    age: 29,
+};
+person.isProgrammer = true;
+console.log(typeof person);
+console.log(person);
 //# sourceMappingURL=index.js.map
--- 00 - Intro/PH/src/index.ts
+++ 00 - Intro/PH/src/index.ts
@@ -24,4 +24,80 @@ let empty;
 console.log(empty);
 
 let a: number = 4;
-a = 'O_o';
+// a = 'O_o';
+// 156-163-175
+
+let shoppingList: string[] = ['egg', 'milk', 'tea'];
+
+let mixedArr: (string | number | boolean)[] = [
+  'egg',
+  12,
+  'milk',
+  1,
+  'suger',
+  5,
+];
+mixedArr.push(true);
+
+// Tuple
+let coord: [number, number] = [20, 30];
+
+let couple: [string, string] = ['husbend', 'wife'];
+
+let student: [string, number] = ['Sakib', 17];
+
+console.log(student[0]);
+student[0] = 'O_o';
+console.log(student[0]);
+
+let emptyArray = [];
+
+// 07 - Objects
+let obj: {
+  readonly id: number;
+  name: string;
+  retireDate?: (date: Date) => void;
+} = {
+  id: 1,
+  name: '',
+  retireDate: (date: Date) => {
+    console.log(date);
+  },
+};
+
+console.log(obj);
+
+type Person = {
+  readonly id: number;
+  name: string;
+  age: number;
+  isProgrammer?: boolean;
+};
+
+const person: Person = {
+  id: 15,
+  name: 'Sakib',
+  age: 29,
+} as const;
+person.isProgrammer = true;
+
+console.log(typeof person);
+
+console.log(person);
+
+type finalPerson = Readonly<Person>;
+
+//
+type NumArray = readonly number[];
+
+const nums: NumArray = [1, 2, 3];
+
+//
+type person = {
+  name: string;
+  age: number;
+};
+
+function getValue(key: keyof Person, person: Person) {
+  return person[key];
+}
