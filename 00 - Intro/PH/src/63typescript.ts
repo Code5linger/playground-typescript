f47c5d4214755462fccc47dad61dde52e9f03a07
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-11-13 01:45:17 +0600

    !

---
+++ 00 - Intro/PH/src/63typescript.ts
@@ -0,0 +1,9 @@
+type GenericArray = Array<string>;
+
+// const friends: string[] = ['A', 'B', 'C'];
+const friends: GenericArray = ['A', 'B', 'C'];
+
+// const rollNumber: number[] = [4, 7, 11];
+const rollNumber: GenericArray = [4, 7, 11];
+
+const isEligible: Array<boolean> = [true, true, false];
