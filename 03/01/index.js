b5ca97775a71f22563088479be5ca616621409a8
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-11-18 13:54:21 +0600

    !

--- 03/01/index.js
+++ 03/01/index.js
@@ -1,8 +1,29 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Account = /** @class */ (function () {
+    // public id: number;
+    // public owner: string;
+    // public balance: number;
     function Account(id, owner, balance) {
         this.id = id;
         this.owner = owner;
         this.balance = balance;
+        // this.id = id;
+        // this.owner = owner;
+        // this.balance = balance;
     }
     Account.prototype.deposit = function (amount) {
         if (amount <= 0)
@@ -14,6 +35,8 @@ var Account = /** @class */ (function () {
 var accountA = new Account(1, 'Me', 0);
 console.log(accountA);
 accountA.deposit(69);
+console.log(accountA);
+accountA.deposit(100);
 console.log(accountA);
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // class Animal {
@@ -33,6 +56,7 @@ console.log(accountA);
 //     );
 //   }
 // }
+// Parameter property
 var Animal = /** @class */ (function () {
     function Animal(id, type, sound, pet) {
         this.id = id;
@@ -51,3 +75,50 @@ dog.logger('ZS');
 var cat = new Animal(2, '😺', '🐈‍⬛', false);
 console.log(cat);
 cat.logger('👋');
+var Car = /** @class */ (function () {
+    function Car(make, model, year, electric) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.electric = electric;
+    }
+    Car.prototype.info = function () {
+        console.log("".concat(this.make, ", ").concat(this.model, ", ").concat(this.year, ", ").concat(this.electric));
+    };
+    return Car;
+}());
+var tesla = new Car('2009', '3X', 2010, true);
+tesla.info();
+var BankAccount = /** @class */ (function () {
+    function BankAccount(userId, userName, userBalance) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userBalance = userBalance;
+    }
+    BankAccount.prototype.updateBalance = function (balance) {
+        this.userBalance = this.userBalance + balance;
+    };
+    BankAccount.prototype.getBalance = function () {
+        return this.userBalance;
+    };
+    return BankAccount;
+}());
+var StudentBankAC = /** @class */ (function (_super) {
+    __extends(StudentBankAC, _super);
+    function StudentBankAC() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    StudentBankAC.prototype.test = function (update) {
+        this.userBalance += update;
+    };
+    return StudentBankAC;
+}(BankAccount));
+var myAccount = new BankAccount(123, 'Sakib', 100);
+// myAccount.updateBalance(20);
+// console.log((myAccount.userBalance += 100));
+console.log(myAccount);
+var myStudentAC = new StudentBankAC(456, 'O_o', 50);
+myStudentAC.test(20);
+console.log(myStudentAC);
+console.log(myAccount.getBalance());
+console.log(myStudentAC.getBalance());
--- 03/01/index.ts
+++ 03/01/index.ts
@@ -1,13 +1,9 @@
 class Account {
-  id: number;
-  owner: string;
-  balance: number;
-
-  constructor(id: number, owner: string, balance: number) {
-    this.id = id;
-    this.owner = owner;
-    this.balance = balance;
-  }
+  constructor(
+    public id: number,
+    public owner: string,
+    public balance: number
+  ) {}
 
   deposit(amount: number) {
     if (amount <= 0) return 'Invalid Number';
@@ -19,6 +15,8 @@ class Account {
 const accountA = new Account(1, 'Me', 0);
 console.log(accountA);
 accountA.deposit(69);
+console.log(accountA);
+accountA.deposit(100);
 console.log(accountA);
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -66,3 +64,62 @@ dog.logger('ZS');
 let cat = new Animal(2, '😺', '🐈‍⬛', false);
 console.log(cat);
 cat.logger('👋');
+
+class Car {
+  make: string;
+  model: string;
+  year: number;
+  electric: boolean;
+
+  constructor(make: string, model: string, year: number, electric: boolean) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.electric = electric;
+  }
+
+  info() {
+    console.log(`${this.make}, ${this.model}, ${this.year}, ${this.electric}`);
+  }
+}
+
+let tesla = new Car('2009', '3X', 2010, true);
+tesla.info();
+
+class BankAccount {
+  public readonly userId: number;
+  public userName: string;
+  protected userBalance: number;
+
+  constructor(userId: number, userName: string, userBalance: number) {
+    this.userId = userId;
+    this.userName = userName;
+    this.userBalance = userBalance;
+  }
+
+  private updateBalance(balance: number) {
+    this.userBalance = this.userBalance + balance;
+  }
+
+  getBalance(): number {
+    return this.userBalance;
+  }
+}
+
+class StudentBankAC extends BankAccount {
+  test(update: number) {
+    this.userBalance += update;
+  }
+}
+
+const myAccount = new BankAccount(123, 'Sakib', 100);
+// myAccount.updateBalance(20);
+// console.log((myAccount.userBalance += 100));
+console.log(myAccount);
+
+const myStudentAC = new StudentBankAC(456, 'O_o', 50);
+myStudentAC.test(20);
+console.log(myStudentAC);
+
+console.log(myAccount.getBalance());
+console.log(myStudentAC.getBalance());
